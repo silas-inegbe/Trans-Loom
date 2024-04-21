d2b8d1e338d0c328b0a4ae399ead95b1176786ce
silas-inegbe <kingssilas18@gmail.com>
2024-04-21 23:54:49 +0100

    supabase key defined

--- app/lib/helper/supabaseClient.js
+++ app/lib/helper/supabaseClient.js
@@ -9,4 +9,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://gtdtyawmlhltmzqpfris.supabase.co'
 const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
